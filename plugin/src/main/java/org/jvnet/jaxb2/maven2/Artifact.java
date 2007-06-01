ab1b1278593ecbf4b0feda8d357f9eb02e015b5f
lexi <lexi@2a132e0c-f51c-0410-b090-b6bda6d2de2a>
2007-06-01 12:00:39 +0000

    Episodoc compilation.

--- plugin/src/main/java/org/jvnet/jaxb2/maven2/Artifact.java
+++ plugin/src/main/java/org/jvnet/jaxb2/maven2/Artifact.java
@@ -6,35 +6,38 @@ import org.apache.maven.artifact.factory.ArtifactFactory;
  * @author Kohsuke Kawaguchi
  */
 public class Artifact {
-    private String groupId;
-    private String artifactId;
-    private String version;
+	private String groupId;
 
-    public String getGroupId() {
-        return groupId;
-    }
+	private String artifactId;
 
-    public void setGroupId(String groupId) {
-        this.groupId = groupId;
-    }
+	private String version;
 
-    public String getArtifactId() {
-        return artifactId;
-    }
+	public String getGroupId() {
+		return groupId;
+	}
 
-    public void setArtifactId(String artifactId) {
-        this.artifactId = artifactId;
-    }
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
+	}
 
-    public String getVersion() {
-        return version;
-    }
+	public String getArtifactId() {
+		return artifactId;
+	}
 
-    public void setVersion(String version) {
-        this.version = version;
-    }
+	public void setArtifactId(String artifactId) {
+		this.artifactId = artifactId;
+	}
 
-    public org.apache.maven.artifact.Artifact toArtifact(ArtifactFactory factory) {
-        return factory.createArtifact(groupId,artifactId,version,null,"jar");
-    }
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public org.apache.maven.artifact.Artifact toArtifact(ArtifactFactory factory) {
+		return factory
+				.createArtifact(groupId, artifactId, version, null, "jar");
+	}
 }

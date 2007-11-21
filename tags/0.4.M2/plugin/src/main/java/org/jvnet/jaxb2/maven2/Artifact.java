cf4cc38acf17babe4cdf0ad509f2d3dae70c3720
lexi <lexi@2a132e0c-f51c-0410-b090-b6bda6d2de2a>
2007-11-21 12:11:55 +0000

    Hudson automatic tag add

---
+++ tags/0.4.M2/plugin/src/main/java/org/jvnet/jaxb2/maven2/Artifact.java
@@ -0,0 +1,43 @@
+package org.jvnet.jaxb2.maven2;
+
+import org.apache.maven.artifact.factory.ArtifactFactory;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class Artifact {
+	private String groupId;
+
+	private String artifactId;
+
+	private String version;
+
+	public String getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
+	}
+
+	public String getArtifactId() {
+		return artifactId;
+	}
+
+	public void setArtifactId(String artifactId) {
+		this.artifactId = artifactId;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public org.apache.maven.artifact.Artifact toArtifact(ArtifactFactory factory) {
+		return factory
+				.createArtifact(groupId, artifactId, version, null, "jar");
+	}
+}
---
+++ tags/0.4.M2/release.bat
@@ -0,0 +1,1 @@
+call mvn -DperformRelease=true clean deploy

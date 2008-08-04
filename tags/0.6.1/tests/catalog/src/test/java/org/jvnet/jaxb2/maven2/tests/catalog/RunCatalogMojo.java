c35e00cb92b0229961d8fa9fb33633439684c49a
lexi <lexi@2a132e0c-f51c-0410-b090-b6bda6d2de2a>
2008-08-04 07:18:40 +0000

    Hudson automatic tag add

---
+++ tags/0.6.1/tests/catalog/src/test/java/org/jvnet/jaxb2/maven2/tests/catalog/RunCatalogMojo.java
@@ -0,0 +1,23 @@
+package org.jvnet.jaxb2.maven2.tests.catalog;
+
+import java.io.File;
+
+import org.jvnet.jaxb2.maven2.XJC2Mojo;
+import org.jvnet.jaxb2.maven2.resolver.tools.ClasspathCatalogResolver;
+import org.jvnet.jaxb2.maven2.test.RunXJC2Mojo;
+
+public class RunCatalogMojo extends RunXJC2Mojo {
+
+	@Override
+	protected void configureMojo(XJC2Mojo mojo) {
+		super.configureMojo(mojo);
+		
+		mojo
+				.setCatalog(new File(getBaseDir(),
+						"src/main/resources/catalog.cat"));
+		mojo.setCatalogResolver(ClasspathCatalogResolver.class.getName());
+		
+		mojo.setForceRegenerate(true);
+	}
+
+}

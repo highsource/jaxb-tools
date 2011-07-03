635a8e649e2d56298e13785dc1d57137c0fc86eb
lexi <lexi@6ed13606-9e1b-ccdd-a29f-aa03f5ca764e>
2011-07-03 14:08:49 +0000

    Version 0.6.2

--- basics/trunk/release.bat
+++ basics/trunk/release.bat
@@ -1,4 +1,5 @@
-setlocal
-set JAVA_HOME=%JAVA5_HOME%
-call mvn -Psonatype-oss-release -DperformRelease clean deploy
-endlocal
+rem update versions
+mvn clean install -Psamples -Ptests
+mvn scm:tag -Dtag=%1
+mvn -Psamples -Ptests -DperformRelease -Psonatype-oss-release clean deploy
+rem update versions
--- basics/trunk/update-jaxb2-commons-versions.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call update-versions ../jaxb2-basics %1 %2

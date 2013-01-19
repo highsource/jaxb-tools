d5fef6b4cd8b6e444ca50c58d01fe97cf0623514
lexi <lexi@2a132e0c-f51c-0410-b090-b6bda6d2de2a>
2013-01-19 22:23:06 +0000

    MAVEN_JAXB2_PLUGIN-63

--- trunk/plugin-2.0/src/main/java/org/jvnet/mjiip/v_2_0/XJC20Mojo.java
+++ trunk/plugin-2.0/src/main/java/org/jvnet/mjiip/v_2_0/XJC20Mojo.java
@@ -10,6 +10,7 @@ import java.util.Iterator;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.jfrog.maven.annomojo.annotations.MojoGoal;
 import org.jfrog.maven.annomojo.annotations.MojoPhase;
+import org.jfrog.maven.annomojo.annotations.MojoRequiresDependencyResolution;
 import org.jvnet.jaxb2.maven2.RawXJC2Mojo;
 
 import com.sun.codemodel.CodeWriter;
@@ -28,6 +29,7 @@ import com.sun.tools.xjc.outline.Outline;
  */
 @MojoGoal("generate")
 @MojoPhase("generate-sources")
+@MojoRequiresDependencyResolution
 public class XJC20Mojo extends RawXJC2Mojo<Options> {
 
 	private final org.jvnet.jaxb2.maven2.OptionsFactory<Options> optionsFactory = new OptionsFactory();

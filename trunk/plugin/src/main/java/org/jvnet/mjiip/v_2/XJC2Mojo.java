5abface15bea2fcde1e518b7586f3965f89eaa10
lexi <lexi@2a132e0c-f51c-0410-b090-b6bda6d2de2a>
2013-01-21 22:19:50 +0000

    MAVEN_JAXB2_PLUGIN-45 Now @threadSafe

--- trunk/plugin/src/main/java/org/jvnet/mjiip/v_2/XJC2Mojo.java
+++ trunk/plugin/src/main/java/org/jvnet/mjiip/v_2/XJC2Mojo.java
@@ -3,6 +3,7 @@ package org.jvnet.mjiip.v_2;
 import org.jfrog.maven.annomojo.annotations.MojoGoal;
 import org.jfrog.maven.annomojo.annotations.MojoPhase;
 import org.jfrog.maven.annomojo.annotations.MojoRequiresDependencyResolution;
+import org.jfrog.maven.annomojo.annotations.MojoThreadSafe;
 import org.jvnet.mjiip.v_2_2.XJC22Mojo;
 
 import com.sun.tools.xjc.Options;
@@ -15,6 +16,7 @@ import com.sun.tools.xjc.Options;
 @MojoGoal("generate")
 @MojoPhase("generate-sources")
 @MojoRequiresDependencyResolution("compile")
+@MojoThreadSafe
 public class XJC2Mojo extends XJC22Mojo {
 
 	private final org.jvnet.jaxb2.maven2.OptionsFactory<Options> optionsFactory = new OptionsFactory();

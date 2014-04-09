585b07b57d8f0272280d0e1967e27c7a63c894c4
lexi <lexi@2a132e0c-f51c-0410-b090-b6bda6d2de2a>
2014-04-09 21:49:11 +0000

    [maven-scm] copy for tag 0.9.0

--- release.bat
+++ release.bat
@@ -1,10 +1,10 @@
-call mvn clean install -Psamples
+call mvn clean install -Psamples,tests
 pause
 call mvn versions:set -Dversions -DnewVersion=%1
 pause
 call mvn versions:commit -Dversions
 pause
-call mvn clean install -Psamples
+call mvn clean install -Psamples,tests
 pause
 call mvn clean install -Psamples,sonatype-oss-release
 pause
@@ -12,7 +12,7 @@ call mvn scm:checkin -Dmessage="Version %1"
 pause
 call mvn scm:tag -Dtag=%1
 pause
-call mvn clean deploy -Psamples,sonatype-oss-release
+call mvn clean deploy -Psonatype-oss-release
 pause
 call mvn versions:set -Dversions -DnewVersion=%2
 pause

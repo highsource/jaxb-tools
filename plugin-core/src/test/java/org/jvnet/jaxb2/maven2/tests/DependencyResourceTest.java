3c33e1ef7b85f625edd49d9fb9807b0432706f0c
highsource <aleksei.valikov@gmail.com>
2014-10-03 12:59:29 +0200

    Minor changes

--- plugin-core/src/test/java/org/jvnet/jaxb2/maven2/tests/DependencyResourceTest.java
+++ plugin-core/src/test/java/org/jvnet/jaxb2/maven2/tests/DependencyResourceTest.java
@@ -9,63 +9,78 @@ public class DependencyResourceTest {
 	public void correctlyParsesDepenencyResource() {
 		
 		{
+			@SuppressWarnings("unused")
 			DependencyResource dependencyResource = DependencyResource
 					.valueOf("a:b");
 		}
 		{
+			@SuppressWarnings("unused")
 			DependencyResource dependencyResource = DependencyResource
 					.valueOf("a:b:");
 		}
 		{
+			@SuppressWarnings("unused")
 			DependencyResource dependencyResource = DependencyResource
 					.valueOf("a:b:c");
 		}
 		{
+			@SuppressWarnings("unused")
 			DependencyResource dependencyResource = DependencyResource
 					.valueOf("a:b:c:");
 		}
 		{
+			@SuppressWarnings("unused")
 			DependencyResource dependencyResource = DependencyResource
 					.valueOf("a:b:c:d");
 		}
 		{
+			@SuppressWarnings("unused")
 			DependencyResource dependencyResource = DependencyResource
 					.valueOf("a:b:c:d:");
 		}
 		{
+			@SuppressWarnings("unused")
 			DependencyResource dependencyResource = DependencyResource
 					.valueOf("a:b:c:d:e");
 		}
 
 		{
+			@SuppressWarnings("unused")
 			DependencyResource dependencyResource = DependencyResource
 					.valueOf("a:b!/c");
 		}
 		{
+			@SuppressWarnings("unused")
 			DependencyResource dependencyResource = DependencyResource
 					.valueOf("a:b:!/c");
 		}
 		{
+			@SuppressWarnings("unused")
 			DependencyResource dependencyResource = DependencyResource
 					.valueOf("a:b:c!/d");
 		}
 		{
+			@SuppressWarnings("unused")
 			DependencyResource dependencyResource = DependencyResource
 					.valueOf("a:b:c:!/d");
 		}
 		{
+			@SuppressWarnings("unused")
 			DependencyResource dependencyResource = DependencyResource
 					.valueOf("a:b:c:d!/e");
 		}
 		{
+			@SuppressWarnings("unused")
 			DependencyResource dependencyResource = DependencyResource
 					.valueOf("a:b::d!/e");
 		}
 		{
+			@SuppressWarnings("unused")
 			DependencyResource dependencyResource = DependencyResource
 					.valueOf("a:b:c:d:!/e");
 		}
 		{
+			@SuppressWarnings("unused")
 			DependencyResource dependencyResource = DependencyResource
 					.valueOf("a:b:c:d:e!/f");
 		}

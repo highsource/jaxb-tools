e4c4942000520ba626cd9281cc5c0184ef86bbd2
highsource <aleksei.valikov@gmail.com>
2014-10-03 13:33:12 +0200

    Commented out debug statements.

--- plugin-core/src/main/java/org/jvnet/jaxb2/maven2/resolver/tools/ClasspathCatalogResolver.java
+++ plugin-core/src/main/java/org/jvnet/jaxb2/maven2/resolver/tools/ClasspathCatalogResolver.java
@@ -3,7 +3,6 @@ package org.jvnet.jaxb2.maven2.resolver.tools;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.text.MessageFormat;
 
 public class ClasspathCatalogResolver extends
 		com.sun.org.apache.xml.internal.resolver.tools.CatalogResolver {
@@ -12,14 +11,14 @@ public class ClasspathCatalogResolver extends
 
 	@Override
 	public String getResolvedEntity(String publicId, String systemId) {
-		System.out.println("Resolving [" + publicId + "], [" + systemId + "].");
+//		System.out.println("Resolving [" + publicId + "], [" + systemId + "].");
 		final String result = super.getResolvedEntity(publicId, systemId);
-		System.out.println("Resolved to [" + result+ "].");
+//		System.out.println("Resolved to [" + result+ "].");
 
 		if (result == null) {
-			System.err.println(MessageFormat.format(
-					"Could not resolve publicId [{0}], systemId [{1}]",
-					publicId, systemId));
+//			System.err.println(MessageFormat.format(
+//					"Could not resolve publicId [{0}], systemId [{1}]",
+//					publicId, systemId));
 			return null;
 		}
 
@@ -27,24 +26,24 @@ public class ClasspathCatalogResolver extends
 			final URI uri = new URI(result);
 			if (URI_SCHEME_CLASSPATH.equals(uri.getScheme())) {
 				final String schemeSpecificPart = uri.getSchemeSpecificPart();
-				System.out.println("Resolve [" + schemeSpecificPart + "].");
+//				System.out.println("Resolve [" + schemeSpecificPart + "].");
 
 				final URL resource = Thread.currentThread()
 						.getContextClassLoader()
 						.getResource(schemeSpecificPart);
 				if (resource == null) {
-					System.out.println("Returning [" + null + "].");
+//					System.out.println("Returning [" + null + "].");
 					return null;
 				} else {
-					System.out.println("Returning to [" + resource.toString()+ "].");
+//					System.out.println("Returning to [" + resource.toString()+ "].");
 					return resource.toString();
 				}
 			} else {
-				System.out.println("Returning to [" + result+ "].");
+//				System.out.println("Returning to [" + result+ "].");
 				return result;
 			}
 		} catch (URISyntaxException urisex) {
-			System.out.println("Returning to [" + result+ "].");
+//			System.out.println("Returning to [" + result+ "].");
 			return result;
 		}
 	}

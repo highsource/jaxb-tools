c8c8f68338b08826dea72257ac38a9125a94d76b
highsource <aleksei.valikov@gmail.com>
2014-10-16 08:24:22 +0200

    Issue #12.

---
+++ ejb/tests/gh-issue-12/src/test/java/org/jvnet/hyperjaxb3/ejb/tests/gh_issue_12/RunPoPlugin.java
@@ -0,0 +1,7 @@
+package org.jvnet.hyperjaxb3.ejb.tests.gh_issue_12;
+
+import org.jvnet.hyperjaxb3.maven2.ejb.test.RunEjbHyperjaxb3Mojo;
+
+public class RunPoPlugin extends RunEjbHyperjaxb3Mojo {
+
+}

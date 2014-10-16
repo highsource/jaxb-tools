c07da736a4ffbe75d585d83773b90a9382178f86
highsource <aleksei.valikov@gmail.com>
2014-10-16 19:49:13 +0200

    Test project removed.

--- ejb/tests/gh-issue-12/src/test/java/org/jvnet/hyperjaxb3/ejb/tests/gh_issue_12/RunPoPlugin.java
+++
@@ -1,7 +0,0 @@
-package org.jvnet.hyperjaxb3.ejb.tests.gh_issue_12;
-
-import org.jvnet.hyperjaxb3.maven2.ejb.test.RunEjbHyperjaxb3Mojo;
-
-public class RunPoPlugin extends RunEjbHyperjaxb3Mojo {
-
-}

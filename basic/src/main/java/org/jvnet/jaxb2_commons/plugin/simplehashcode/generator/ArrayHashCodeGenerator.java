c2c22bebcc2e601163cf1a235857aada9a05bac8
highsource <aleksei.valikov@gmail.com>
2014-12-03 08:39:04 +0100

    Issue #11. Just delegate to Arrays.hashCode(...) for arrays.

--- basic/src/main/java/org/jvnet/jaxb2_commons/plugin/simplehashcode/generator/ArrayHashCodeGenerator.java
+++ basic/src/main/java/org/jvnet/jaxb2_commons/plugin/simplehashcode/generator/ArrayHashCodeGenerator.java
@@ -1,31 +1,28 @@
 package org.jvnet.jaxb2_commons.plugin.simplehashcode.generator;
 
-import java.util.Collection;
+import java.util.Arrays;
 
-import com.sun.codemodel.JBlock;
 import com.sun.codemodel.JCodeModel;
-import com.sun.codemodel.JExpr;
-import com.sun.codemodel.JForEach;
+import com.sun.codemodel.JExpression;
 import com.sun.codemodel.JType;
 import com.sun.codemodel.JVar;
 
-public class ArrayHashCodeGenerator extends BlockHashCodeCodeGenerator {
+public class ArrayHashCodeGenerator extends ValueBasedHashCodeCodeGenerator {
 
-	public ArrayHashCodeGenerator(
-			TypedHashCodeCodeGeneratorFactory factory, JCodeModel codeModel) {
+	public ArrayHashCodeGenerator(TypedHashCodeCodeGeneratorFactory factory,
+			JCodeModel codeModel) {
 		super(factory, codeModel);
+	}
+	
+	@Override
+	protected boolean isCheckForNullRequired() {
+		return false;
 	}
 
 	@Override
-	protected void generate(JBlock block, JVar currentHashCode,
-			JType possibleType, Collection<JType> possibleTypes, JVar value) {
-
-		final JType elementType = possibleType.elementType();
-		final JForEach forEachElement = block.forEach(elementType, value.name()
-				+ "Element", value);
-		getFactory().getCodeGenerator(elementType).generate(
-				forEachElement.body(), currentHashCode, elementType,
-				possibleTypes, forEachElement.var(), JExpr.TRUE, true);
+	protected JExpression valueHashCode(JType type, JVar value) {
+		return getCodeModel().ref(Arrays.class).staticInvoke("hashCode")
+				.arg(value);
 	}
 
 }

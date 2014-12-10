ae2da88bca5b0851d4947e33b99d403e81a5e80b
highsource <aleksei.valikov@gmail.com>
2014-12-10 12:27:37 +0100

    Minor changes - some formatting.

--- testing/src/main/java/org/jvnet/jaxb2/maven2/test/RunXJC2Mojo.java
+++ testing/src/main/java/org/jvnet/jaxb2/maven2/test/RunXJC2Mojo.java
@@ -20,70 +20,67 @@ import org.jvnet.mjiip.v_2.XJC2Mojo;
  */
 
 public class RunXJC2Mojo extends TestCase {
-  /**
-   * Logger.
-   */
-  protected Log log = LogFactory.getLog(RunXJC2Mojo.class);
+	/**
+	 * Logger.
+	 */
+	protected Log log = LogFactory.getLog(RunXJC2Mojo.class);
 
-  public void testExecute() throws Exception {
-    final Mojo mojo = initMojo();
-    mojo.execute();
-  }
-  
-  public void check() throws Exception
-  {    
-  }
+	public void testExecute() throws Exception {
+		final Mojo mojo = initMojo();
+		mojo.execute();
+	}
 
-  protected File getBaseDir() {
-    try {
-      return (new File(getClass().getProtectionDomain().getCodeSource().getLocation().toURI()))
-          .getParentFile()
-          .getParentFile()
-          .getAbsoluteFile();
-    }
-    catch (Exception ex) {
-      throw new AssertionError(ex);
-    }
-  }
+	public void check() throws Exception {
+	}
 
-  public File getSchemaDirectory() {
-    return new File(getBaseDir(), "src/main/resources");
-  }
+	protected File getBaseDir() {
+		try {
+			return (new File(getClass().getProtectionDomain().getCodeSource()
+					.getLocation().toURI())).getParentFile().getParentFile()
+					.getAbsoluteFile();
+		} catch (Exception ex) {
+			throw new AssertionError(ex);
+		}
+	}
 
-  protected File getGeneratedDirectory() {
-    return new File(getBaseDir(), "target/generated-sources/xjc");
-  }
+	public File getSchemaDirectory() {
+		return new File(getBaseDir(), "src/main/resources");
+	}
 
-  public List<String> getArgs() {
-    return Collections.emptyList();
-  }
+	protected File getGeneratedDirectory() {
+		return new File(getBaseDir(), "target/generated-sources/xjc");
+	}
 
-  public String getGeneratePackage() {
-    return null;
-  }
+	public List<String> getArgs() {
+		return Collections.emptyList();
+	}
 
-  public boolean isWriteCode() {
-    return true;
-  }
+	public String getGeneratePackage() {
+		return null;
+	}
 
-  public AbstractXJC2Mojo initMojo() {
-    final AbstractXJC2Mojo mojo = createMojo();
-    configureMojo(mojo);
-    return mojo;
-  }
+	public boolean isWriteCode() {
+		return true;
+	}
 
-  protected AbstractXJC2Mojo createMojo() {
-    return new XJC2Mojo();
-  }
+	public AbstractXJC2Mojo initMojo() {
+		final AbstractXJC2Mojo mojo = createMojo();
+		configureMojo(mojo);
+		return mojo;
+	}
 
-  protected void configureMojo(final AbstractXJC2Mojo mojo) {
-	mojo.setProject(new MavenProject());
-    mojo.setSchemaDirectory(getSchemaDirectory());
-    mojo.setGenerateDirectory(getGeneratedDirectory());
-    mojo.setGeneratePackage(getGeneratePackage());
-    mojo.setArgs(getArgs());
-    mojo.setVerbose(true);
-    mojo.setDebug(true);
-    mojo.setWriteCode(isWriteCode());
-  }
+	protected AbstractXJC2Mojo createMojo() {
+		return new XJC2Mojo();
+	}
+
+	protected void configureMojo(final AbstractXJC2Mojo mojo) {
+		mojo.setProject(new MavenProject());
+		mojo.setSchemaDirectory(getSchemaDirectory());
+		mojo.setGenerateDirectory(getGeneratedDirectory());
+		mojo.setGeneratePackage(getGeneratePackage());
+		mojo.setArgs(getArgs());
+		mojo.setVerbose(true);
+		mojo.setDebug(true);
+		mojo.setWriteCode(isWriteCode());
+	}
 }

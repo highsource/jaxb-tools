1e7ca449658b89d7caa98b24da2d4ce213f4f5f3
highsource <aleksei.valikov@gmail.com>
2014-12-19 11:39:28 +0100

    Issue #11. Added as sample project.

---
+++ samples/po-simple/src/test/java/org/jvnet/jaxb2_commons/tests/posimple/EqualsTest.java
@@ -0,0 +1,21 @@
+package org.jvnet.jaxb2_commons.tests.posimple;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBElement;
+
+import org.jvnet.jaxb2_commons.test.AbstractSamplesTest;
+
+public class EqualsTest extends AbstractSamplesTest {
+
+	@Override
+	protected void checkSample(File sample) throws Exception {
+
+		final JAXBElement<?> lhs = (JAXBElement<?>) createContext()
+				.createUnmarshaller().unmarshal(sample);
+		final JAXBElement<?> rhs = (JAXBElement<?>) createContext()
+				.createUnmarshaller().unmarshal(sample);
+		assertTrue("Values must be equal.",
+				lhs.getValue().equals(rhs.getValue()));
+	}
+}

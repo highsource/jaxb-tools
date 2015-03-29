304a7beeb0682fcc49c9e0d8c9ce42aae12297db
highsource <aleksei.valikov@gmail.com>
2015-03-29 21:26:55 +0200

    Test for issue #23.

---
+++ tools/src/test/java/org/jvnet/jaxb2_commons/xjc/model/concrete/tests/ahpla/KnownReferencedType.java
@@ -0,0 +1,23 @@
+package org.jvnet.jaxb2_commons.xjc.model.concrete.tests.ahpla;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "KnownReferencedType", propOrder = { "str" })
+public class KnownReferencedType implements Serializable {
+	protected String str;
+
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String value) {
+		this.str = value;
+	}
+
+}

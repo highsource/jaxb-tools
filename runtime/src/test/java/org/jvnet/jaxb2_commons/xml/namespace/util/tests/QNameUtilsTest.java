efc779a58725b2dfca326b7323be9bde13414739
highsource <aleksei.valikov@gmail.com>
2015-03-29 12:48:49 +0200

    Issue #23. Added QNameUtils.

---
+++ runtime/src/test/java/org/jvnet/jaxb2_commons/xml/namespace/util/tests/QNameUtilsTest.java
@@ -0,0 +1,19 @@
+package org.jvnet.jaxb2_commons.xml.namespace.util.tests;
+
+import javax.xml.namespace.QName;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.jvnet.jaxb2_commons.xml.namespace.util.QNameUtils;
+
+public class QNameUtilsTest {
+
+	@Test
+	public void producesCorrectKey()
+	{
+		Assert.assertEquals("a", QNameUtils.getKey(new QName("a")));
+		Assert.assertEquals("{b}a", QNameUtils.getKey(new QName("b", "a")));
+		Assert.assertEquals("{b}c:a", QNameUtils.getKey(new QName("b", "a", "c")));
+		Assert.assertEquals("c:a", QNameUtils.getKey(new QName("", "a", "c")));
+	}
+}

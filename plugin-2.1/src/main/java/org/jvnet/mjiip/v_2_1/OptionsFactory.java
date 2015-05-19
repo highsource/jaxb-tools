bba6933f0c04a673d0b3a6f875318b410eda4be8
Dawuid <dawuid@gmail.com>
2015-05-19 09:37:31 +0200

    Proxy configuration in maven (settings) is share with xjc execution.
    Useful for recovering XSD dependencies (in XSD parsing) behind a proxy.

--- plugin-2.1/src/main/java/org/jvnet/mjiip/v_2_1/OptionsFactory.java
+++ plugin-2.1/src/main/java/org/jvnet/mjiip/v_2_1/OptionsFactory.java
@@ -17,7 +17,7 @@ public class OptionsFactory implements
 		org.jvnet.jaxb2.maven2.OptionsFactory<Options> {
 	/**
 	 * Creates and initializes an instance of XJC options.
-	 * 
+	 *
 	 */
 	public Options createOptions(OptionsConfiguration optionsConfiguration)
 			throws MojoExecutionException {
@@ -70,6 +70,16 @@ public class OptionsFactory implements
 
 		if (optionsConfiguration.isExtension()) {
 			options.compatibilityMode = Options.EXTENSION;
+		}
+
+		String proxy = optionsConfiguration.getProxy();
+		if ((proxy != null) && (proxy.length() > 0)) {
+			try {
+				options.parseArguments(new String[] { "-httpproxy", proxy });
+			}
+			catch (BadCommandLineException bclex) {
+				throw new MojoExecutionException("Error parsing the proxy line ", bclex);
+			}
 		}
 
 		final List<String> arguments = optionsConfiguration.getArguments();

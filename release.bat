8e1d2efdf60c9afc44bdbe3da986688be7526366
highsource <aleksei.valikov@gmail.com>
2015-08-28 23:25:42 +0200

    Fixes #12

--- release.bat
+++ release.bat
@@ -1,3 +1,32 @@
+@REM
+@REM Copyright © 2005-2015, Alexey Valikov
+@REM All rights reserved.
+@REM
+@REM Redistribution and use in source and binary forms, with or without
+@REM modification, are permitted provided that the following conditions are met:
+@REM
+@REM 1. Redistributions of source code must retain the above copyright notice, this
+@REM    list of conditions and the following disclaimer.
+@REM 2. Redistributions in binary form must reproduce the above copyright notice,
+@REM    this list of conditions and the following disclaimer in the documentation
+@REM    and/or other materials provided with the distribution.
+@REM
+@REM THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+@REM ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+@REM WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+@REM DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+@REM ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+@REM (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+@REM LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+@REM ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+@REM (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+@REM SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+@REM
+@REM The views and conclusions contained in the software and documentation are those
+@REM of the authors and should not be interpreted as representing official policies,
+@REM either expressed or implied, of the FreeBSD Project.
+@REM
+
 setlocal
 echo Setting JAVA_HOME to %JAVA6_HOME%.
 set JAVA_HOME=%JAVA6_HOME%

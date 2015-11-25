5a8d6a703141f6140d826c7a95c3af097cc22653
highsource <aleksei.valikov@gmail.com>
2015-11-25 20:14:14 +0100

    Issue #24.

--- ejb/tests/build-o-p-1.6.bat
+++
@@ -1,4 +0,0 @@
-setlocal
-set JAVA_HOME=%JAVA6_HOME%
-call mvn -X -Ptests-o-p clean install >std 2>err
-endlocal
--- ejb/tests/build-r-z-1.6.bat
+++
@@ -1,4 +0,0 @@
-setlocal
-set JAVA_HOME=%JAVA6_HOME%
-call mvn -X -Ptests-r-z clean install >std 2>err
-endlocal

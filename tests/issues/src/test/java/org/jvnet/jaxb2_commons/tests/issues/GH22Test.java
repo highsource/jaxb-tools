38670b04f87e2c3d160b1be4e21581c91789c21c
highsource <aleksei.valikov@gmail.com>
2015-12-24 09:57:46 +0100

    Issue #22 seems to be fixed already.

---
+++ tests/issues/src/test/java/org/jvnet/jaxb2_commons/tests/issues/GH22Test.java
@@ -0,0 +1,27 @@
+package org.jvnet.jaxb2_commons.tests.issues;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.jvnet.jaxb2_commons.lang.JAXBEqualsStrategy;
+
+public class GH22Test {
+	@Test
+	public void testJAXBEqualsSymmetryConcreteClassAndEnum() {
+		Assert.assertFalse(JAXBEqualsStrategy.INSTANCE.equals(null, null,
+				new SomeConcreteClass(), SomeEnum.ENUM));
+	}
+
+	@Test
+	public void testJAXBEqualsSymmetryEnumAndConcreteClass() {
+		// This test fails and throws a ClassCastException
+		Assert.assertFalse(JAXBEqualsStrategy.INSTANCE.equals(null, null,
+				SomeEnum.ENUM, new SomeConcreteClass()));
+	}
+
+	private static class SomeConcreteClass {
+	}
+
+	private enum SomeEnum {
+		ENUM;
+	}
+}

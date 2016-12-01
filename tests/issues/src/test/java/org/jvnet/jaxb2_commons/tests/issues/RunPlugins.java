2f45e2dcc99a0fa66446543bd528567978212d2c
highsource <aleksei.valikov@gmail.com>
2016-12-01 20:46:42 +0100

    Experiments #77.

--- tests/issues/src/test/java/org/jvnet/jaxb2_commons/tests/issues/RunPlugins.java
+++ tests/issues/src/test/java/org/jvnet/jaxb2_commons/tests/issues/RunPlugins.java
@@ -3,14 +3,22 @@ package org.jvnet.jaxb2_commons.tests.issues;
 import java.io.File;
 import java.net.URL;
 
+import javax.xml.namespace.QName;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.jvnet.jaxb2_commons.xjc.model.concrete.XJCCMInfoFactory;
+import org.jvnet.jaxb2_commons.xml.bind.model.MElementInfo;
+import org.jvnet.jaxb2_commons.xml.bind.model.MModelInfo;
+import org.jvnet.jaxb2_commons.xml.bind.model.MPropertyInfo;
 
 import com.sun.codemodel.JCodeModel;
 import com.sun.tools.xjc.ConsoleErrorReporter;
 import com.sun.tools.xjc.ModelLoader;
 import com.sun.tools.xjc.Options;
 import com.sun.tools.xjc.model.Model;
+import com.sun.tools.xjc.model.nav.NClass;
+import com.sun.tools.xjc.model.nav.NType;
 
 public class RunPlugins {
 

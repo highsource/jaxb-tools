1bf4019ab8c61469fb09f520bf2916ca304a6449
Alexey Valikov <aleksei.valikov@gmail.com>
2018-06-03 23:24:58 +0200

    Fixing Ant and Maven/maven-jaxb2-plugin samples for #91.

--- full-build-9.bat
+++ full-build-9.bat
@@ -1,5 +1,5 @@
 setlocal
 echo Setting JAVA_HOME to %JAVA9_HOME%.
 set JAVA_HOME=%JAVA9_HOME%
-call mvn clean install -Pall -DperformRelease
+call mvn clean install --fail-at-end -Pall -DperformRelease 
 endlocal

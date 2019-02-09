a11a239a32721de3a3661e5823d00eb153078ecf
Alexey Valikov <aleksei.valikov@gmail.com>
2019-02-09 21:46:12 +0100

    Removed unnecessary import.

--- tools/src/test/java/org/jvnet/jaxb2_commons/xjc/model/concrete/tests/ahpla/KnownReferencedType.java
+++ tools/src/test/java/org/jvnet/jaxb2_commons/xjc/model/concrete/tests/ahpla/KnownReferencedType.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 @XmlAccessorType(XmlAccessType.FIELD)

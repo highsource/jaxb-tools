76dbf740aa9857273ab02299a2cd3a6d9051a687
Alexey Valikov <aleksei.valikov@gmail.com>
2019-02-09 21:35:04 +0100

    Suppressed a few warnings.

--- tests/simplify-01/src/main/java/org/jvnet/jaxb2_commons/plugin/simplify/tests01/BaseElementRef.java
+++ tests/simplify-01/src/main/java/org/jvnet/jaxb2_commons/plugin/simplify/tests01/BaseElementRef.java
@@ -3,18 +3,19 @@ package org.jvnet.jaxb2_commons.plugin.simplify.tests01;
 import javax.xml.bind.JAXBElement;
 import javax.xml.namespace.QName;
 
-public class BaseElementRef
-    extends JAXBElement<BaseType>
-{
+@SuppressWarnings("serial")
+public class BaseElementRef extends JAXBElement<BaseType> {
 
-    protected final static QName NAME = new QName("urn:test", "baseElement");
+	protected final static QName NAME = new QName("urn:test", "baseElement");
 
-    public BaseElementRef(BaseType value) {
-        super(NAME, ((Class) BaseType.class), null, value);
-    }
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public BaseElementRef(BaseType value) {
+		super(NAME, ((Class) BaseType.class), null, value);
+	}
 
-    public BaseElementRef() {
-        super(NAME, ((Class) BaseType.class), null, null);
-    }
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public BaseElementRef() {
+		super(NAME, ((Class) BaseType.class), null, null);
+	}
 
 }

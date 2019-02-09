f745121d6178b521f6401d2d28f2491f4af3ceb6
Alexey Valikov <aleksei.valikov@gmail.com>
2019-02-09 21:42:06 +0100

    Removed unnecessary imports.

--- tools/src/main/java/org/jvnet/jaxb2_commons/xjc/outline/FieldAccessorEx.java
+++ tools/src/main/java/org/jvnet/jaxb2_commons/xjc/outline/FieldAccessorEx.java
@@ -1,8 +1,6 @@
 package org.jvnet.jaxb2_commons.xjc.outline;
 
-import com.sun.codemodel.JBlock;
 import com.sun.codemodel.JType;
-import com.sun.codemodel.JVar;
 import com.sun.tools.xjc.outline.FieldAccessor;
 
 public interface FieldAccessorEx extends FieldAccessor {

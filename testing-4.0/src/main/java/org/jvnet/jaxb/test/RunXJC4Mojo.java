9a2c91cf17e8d95cc397199268ec51008ae128a6
Laurent SCHOELENS <laurent.schoelens@sfr.com>
2023-02-10 17:57:09 +0100

    add maven testing jaxb3 / jaxb4 modules for jaxb-basics

---
+++ testing-4.0/src/main/java/org/jvnet/jaxb/test/RunXJC4Mojo.java
@@ -0,0 +1,86 @@
+package org.jvnet.jaxb.test;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.apache.maven.plugin.Mojo;
+import org.apache.maven.project.MavenProject;
+import org.jvnet.jaxb.AbstractXJC3Mojo;
+import org.jvnet.mjiip.v_4_0.XJC40Mojo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Abstract test for plugins.
+ * 
+ * @author Aleksei Valikov
+ */
+
+public class RunXJC4Mojo extends TestCase {
+	/**
+	 * Logger.
+	 */
+	protected Logger log = LoggerFactory.getLogger(RunXJC4Mojo.class);
+
+	public void testExecute() throws Exception {
+		final Mojo mojo = initMojo();
+		mojo.execute();
+	}
+
+	public void check() throws Exception {
+	}
+
+	protected File getBaseDir() {
+		try {
+			return (new File(getClass().getProtectionDomain().getCodeSource()
+					.getLocation().toURI())).getParentFile().getParentFile()
+					.getAbsoluteFile();
+		} catch (Exception ex) {
+			throw new AssertionError(ex);
+		}
+	}
+
+	public File getSchemaDirectory() {
+		return new File(getBaseDir(), "src/main/resources");
+	}
+
+	protected File getGeneratedDirectory() {
+		return new File(getBaseDir(), "target/generated-sources/xjc");
+	}
+
+	public List<String> getArgs() {
+		return Collections.emptyList();
+	}
+
+	public String getGeneratePackage() {
+		return null;
+	}
+
+	public boolean isWriteCode() {
+		return true;
+	}
+
+	public AbstractXJC3Mojo initMojo() {
+		final AbstractXJC3Mojo mojo = createMojo();
+		configureMojo(mojo);
+		return mojo;
+	}
+
+	protected AbstractXJC3Mojo createMojo() {
+		return new XJC40Mojo();
+	}
+
+	protected void configureMojo(final AbstractXJC3Mojo mojo) {
+		mojo.setProject(new MavenProject());
+		mojo.setSchemaDirectory(getSchemaDirectory());
+		mojo.setGenerateDirectory(getGeneratedDirectory());
+		mojo.setGeneratePackage(getGeneratePackage());
+		mojo.setArgs(getArgs());
+		mojo.setVerbose(true);
+		mojo.setDebug(true);
+		mojo.setWriteCode(isWriteCode());
+	}
+}

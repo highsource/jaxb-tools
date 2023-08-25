6d230935c86895caa24aeb401f2044506ea40614
Laurent SCHOELENS <61973605+laurentschoelens@users.noreply.github.com>
2023-08-25 12:49:40 +0200

    Fixing issue JAP-9 : unescape string annox that get quoted after by codemodel

--- jaxb-annox-parent/jaxb-annox/src/main/java/org/jvnet/jaxb/annox/model/annotation/value/XStringAnnotationValue.java
+++ jaxb-annox-parent/jaxb-annox/src/main/java/org/jvnet/jaxb/annox/model/annotation/value/XStringAnnotationValue.java
@@ -1,9 +1,13 @@
 package org.jvnet.jaxb.annox.model.annotation.value;
 
+import org.apache.commons.text.StringEscapeUtils;
+
 public class XStringAnnotationValue extends XStaticAnnotationValue<String> {
 
 	public XStringAnnotationValue(String value) {
-		super(value);
+        // fix JAP-9 issue : \ needs to be espaced for javaparser to work
+        // but will then be escaped again by codemodel to generate annotation
+        super(StringEscapeUtils.unescapeJava(value));
 	}
 
 	@Override

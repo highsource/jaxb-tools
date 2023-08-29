393ce87eb972a46de7e0c8df623b2f28afa4c5f2
Laurent SCHOELENS <61973605+laurentschoelens@users.noreply.github.com>
2023-08-30 00:25:52 +0200

    [#188] Classloaders not being closed for mojo classloader

--- maven-plugin/plugin-core/src/main/java/org/jvnet/jaxb/maven/util/IOUtils.java
+++ maven-plugin/plugin-core/src/main/java/org/jvnet/jaxb/maven/util/IOUtils.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;

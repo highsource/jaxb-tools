c233c87f62823a7a0b1343d2e461a61e348079b6
Laurent SCHOELENS <61973605+laurentschoelens@users.noreply.github.com>
2023-09-01 21:42:49 +0200

    fix javadoc

--- hyperjaxb/ejb/plugin/src/main/java/org/jvnet/hyperjaxb3/ejb/strategy/model/CreatePropertyInfos.java
+++ hyperjaxb/ejb/plugin/src/main/java/org/jvnet/hyperjaxb3/ejb/strategy/model/CreatePropertyInfos.java
@@ -12,14 +12,14 @@ public interface CreatePropertyInfos extends
 
 	/**
 	 * Creates a collection of properties for the given property.
-	 * 
+	 *
 	 * @param context
 	 *            processing context.
 	 * @param propertyInfo
 	 *            property to be processed.
 	 * @return Collection of properties created for the given property. Must not
 	 *         be
-	 *         <code><code>null</code>, if nothing is created, return an empty collection instead.
+	 *         <code>null</code>, if nothing is created, return an empty collection instead.
 	 */
 	public Collection<CPropertyInfo> process(ProcessModel context,
 			CPropertyInfo propertyInfo);

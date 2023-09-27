ab57dba1a0a9239c3d4977b9895069389007d27e
Matt Pavlovich <matt@hyte.io>
2023-09-27 13:13:54 -0500

    [#401] Add inheritence unit test w/ modern namespace

--- jaxb-plugins-parent/jaxb-plugins/src/main/java/org/jvnet/jaxb/plugin/inheritance/ObjectFactory.java
+++ jaxb-plugins-parent/jaxb-plugins/src/main/java/org/jvnet/jaxb/plugin/inheritance/ObjectFactory.java
@@ -4,6 +4,18 @@ import jakarta.xml.bind.annotation.XmlRegistry;
 
 @XmlRegistry
 public class ObjectFactory {
+  
+    public ExtendsClass createExtendsClass() {
+        return new ExtendsClass();
+    }
+  
+    public ImplementsInterface createImplementsInterface() {
+        return new ImplementsInterface();
+    }
+  
+    public ObjectFactoryCustomization createObjectFactoryCustomization() {
+        return new ObjectFactoryCustomization();
+    }
 
     // TODO: [#403] jt-403 Remove support for deprecated legacy jaxb2-commons xml namespace  
 	public LegacyExtendsClass createLegacyExtendsClass() {

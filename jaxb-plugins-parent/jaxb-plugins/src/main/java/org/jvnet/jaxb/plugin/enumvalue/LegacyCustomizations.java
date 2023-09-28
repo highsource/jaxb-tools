3d6f3bdbd5c5fd113b8909dfb8f342b44741f3af
Matt Pavlovich <matt@hyte.io>
2023-09-28 10:23:27 -0500

    [#401] Update enumvalue plugin with modern namespace

---
+++ jaxb-plugins-parent/jaxb-plugins/src/main/java/org/jvnet/jaxb/plugin/enumvalue/LegacyCustomizations.java
@@ -0,0 +1,12 @@
+package org.jvnet.jaxb.plugin.enumvalue;
+
+import javax.xml.namespace.QName;
+
+public class LegacyCustomizations {
+
+	public static String NAMESPACE_URI = "http://jaxb2-commons.dev.java.net/basic/enumvalue";
+
+	public static QName IGNORED_ELEMENT_NAME = new QName(NAMESPACE_URI,
+			"ignored");
+
+}

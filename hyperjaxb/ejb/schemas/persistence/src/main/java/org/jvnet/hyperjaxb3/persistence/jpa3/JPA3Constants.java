dcf47d5a7102056c956db3e0247083d1accfc2f0
Laurent SCHOELENS <61973605+laurentschoelens@users.noreply.github.com>
2023-10-06 00:11:59 +0200

    [#425] jpa 3.1 for hyperjaxb with hibernate 6 / eclipselink 4

--- hyperjaxb/ejb/schemas/persistence/src/main/java/org/jvnet/hyperjaxb3/persistence/jpa3/JPA3Constants.java
+++ hyperjaxb/ejb/schemas/persistence/src/main/java/org/jvnet/hyperjaxb3/persistence/jpa3/JPA3Constants.java
@@ -10,6 +10,6 @@ public class JPA3Constants {
 	public static final String SCHEMA_LOCATION = PersistenceConstants.PERSISTENCE_NAMESPACE_URI
 			+ " https://jakarta.ee/xml/ns/persistence/persistence_3_0.xsd\n"
 			+ PersistenceConstants.ORM_NAMESPACE_URI
-			+ " https://jakarta.ee/xml/ns/persistence/orm/orm_3_0.xsd";
+			+ " https://jakarta.ee/xml/ns/persistence/orm/orm_3_1.xsd";
 
 }

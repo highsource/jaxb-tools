94ae123333cfe90c1be3da8f7becb73a9a6ba70b
Laurent SCHOELENS <61973605+laurentschoelens@users.noreply.github.com>
2024-03-29 12:20:51 +0100

    [#529] fix UnaryExpr not taken when parsing negative numbers

--- jaxb-annox-parent/jaxb-annox/src/test/java/org/jvnet/jaxb/annox/parser/tests/Two.java
+++ jaxb-annox-parent/jaxb-annox/src/test/java/org/jvnet/jaxb/annox/parser/tests/Two.java
@@ -18,12 +18,12 @@ package org.jvnet.jaxb.annox.parser.tests;
 			byteArrayField = {0, 1 },
 			charArrayField = { 'a', 'b' },
 			classArrayField = {String.class, Boolean.class },
-			doubleArrayField = { 2, 3 },
+			doubleArrayField = { 2, 3, -3 },
 			enumArrayField = {	E.ONE, E.TWO },
-			floatArrayField = { 4.5f, 6.7f },
-			intArrayField = { 8, 9 },
-			longArrayField = { 10, -9223372036854775808L },
-			shortArrayField = { 12, 13 },
+			floatArrayField = { 4.5f, -6.7f },
+			intArrayField = { 8, -9 },
+			longArrayField = { 10, -10, -9223372036854775808L },
+			shortArrayField = { 12, -13 },
 			stringArrayField = { "14", "15", "16", "17" }))
 public class Two {
 

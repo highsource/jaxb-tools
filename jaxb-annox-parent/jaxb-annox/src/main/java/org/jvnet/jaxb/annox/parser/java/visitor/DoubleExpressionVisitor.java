9a51e49c21eb9762b546049a518d8f47dbdf6ea4
Laurent SCHOELENS <61973605+laurentschoelens@users.noreply.github.com>
2024-03-29 12:20:51 +0100

    [#529] fix UnaryExpr not taken when parsing negative numbers

--- jaxb-annox-parent/jaxb-annox/src/main/java/org/jvnet/jaxb/annox/parser/java/visitor/DoubleExpressionVisitor.java
+++ jaxb-annox-parent/jaxb-annox/src/main/java/org/jvnet/jaxb/annox/parser/java/visitor/DoubleExpressionVisitor.java
@@ -1,5 +1,7 @@
 package org.jvnet.jaxb.annox.parser.java.visitor;
 
+import com.github.javaparser.ast.expr.DoubleLiteralExpr;
+import com.github.javaparser.ast.expr.UnaryExpr;
 import org.jvnet.jaxb.annox.model.annotation.value.XAnnotationValue;
 import org.jvnet.jaxb.annox.model.annotation.value.XDoubleAnnotationValue;
 
@@ -15,4 +17,12 @@ public final class DoubleExpressionVisitor extends
 	public XAnnotationValue<Double> visitDefault(StringLiteralExpr n, Void arg) {
 		return new XDoubleAnnotationValue(Double.valueOf(n.getValue()));
 	}
+
+    @Override
+    public XAnnotationValue<Double> visit(UnaryExpr n, Void arg) {
+        if (n.getExpr() instanceof StringLiteralExpr && n.getOperator() == UnaryExpr.Operator.negative) {
+            return visit(new DoubleLiteralExpr("-" + ((StringLiteralExpr) n.getExpr()).getValue()), arg);
+        }
+        return super.visit(n, arg);
+    }
 }

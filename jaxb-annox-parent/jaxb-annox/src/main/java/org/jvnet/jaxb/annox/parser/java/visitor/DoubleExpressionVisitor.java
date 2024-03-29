e65f8536dd6df14c81df9916e2b5904d6772c964
Laurent SCHOELENS <61973605+laurentschoelens@users.noreply.github.com>
2024-03-29 13:18:35 +0100

    [#529] move to javaparser 3.x latest

--- jaxb-annox-parent/jaxb-annox/src/main/java/org/jvnet/jaxb/annox/parser/java/visitor/DoubleExpressionVisitor.java
+++ jaxb-annox-parent/jaxb-annox/src/main/java/org/jvnet/jaxb/annox/parser/java/visitor/DoubleExpressionVisitor.java
@@ -1,11 +1,10 @@
 package org.jvnet.jaxb.annox.parser.java.visitor;
 
 import com.github.javaparser.ast.expr.DoubleLiteralExpr;
+import com.github.javaparser.ast.expr.LiteralStringValueExpr;
 import com.github.javaparser.ast.expr.UnaryExpr;
 import org.jvnet.jaxb.annox.model.annotation.value.XAnnotationValue;
 import org.jvnet.jaxb.annox.model.annotation.value.XDoubleAnnotationValue;
-
-import com.github.javaparser.ast.expr.StringLiteralExpr;
 
 public final class DoubleExpressionVisitor extends
 		ExpressionVisitor<XAnnotationValue<Double>> {
@@ -14,14 +13,14 @@ public final class DoubleExpressionVisitor extends
 	}
 
 	@Override
-	public XAnnotationValue<Double> visitDefault(StringLiteralExpr n, Void arg) {
+	public XAnnotationValue<Double> visitDefault(LiteralStringValueExpr n, Void arg) {
 		return new XDoubleAnnotationValue(Double.valueOf(n.getValue()));
 	}
 
     @Override
     public XAnnotationValue<Double> visit(UnaryExpr n, Void arg) {
-        if (n.getExpr() instanceof StringLiteralExpr && n.getOperator() == UnaryExpr.Operator.negative) {
-            return visit(new DoubleLiteralExpr("-" + ((StringLiteralExpr) n.getExpr()).getValue()), arg);
+        if (n.getExpression() instanceof LiteralStringValueExpr && n.getOperator() == UnaryExpr.Operator.MINUS) {
+            return visit(new DoubleLiteralExpr("-" + ((LiteralStringValueExpr) n.getExpression()).getValue()), arg);
         }
         return super.visit(n, arg);
     }

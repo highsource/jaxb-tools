e41f7f9c3acfc353a0bdb11583ed91bc7a3f115d
Laurent SCHOELENS <61973605+laurentschoelens@users.noreply.github.com>
2025-04-13 09:36:55 +0200

    fix junit4 tests

--- basics/tests/defaultvalue/src/test/java/org/jvnet/jaxb2_commons/tests/defaultvalue/MathTypesTest.java
+++ basics/tests/defaultvalue/src/test/java/org/jvnet/jaxb2_commons/tests/defaultvalue/MathTypesTest.java
@@ -1,8 +1,8 @@
 package org.jvnet.jaxb2_commons.tests.defaultvalue;
 
 import generated.MathTypes;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -12,11 +12,11 @@ public class MathTypesTest {
     @Test
     public void testMathTypes() {
         MathTypes m = new MathTypes();
-        Assertions.assertEquals(new BigInteger("1000"), m.getBigint());
-        Assertions.assertEquals(new BigInteger("9223372036854775807000"), m.getBigbigint());
-        Assertions.assertEquals(new BigInteger("-1000"), m.getNegbigint());
-        Assertions.assertEquals(new BigDecimal("1.0"), m.getBigdec());
-        Assertions.assertEquals(new BigDecimal("9223372036854775807000.0123456789"), m.getBigbigdec());
-        Assertions.assertEquals(new BigDecimal("-1.0"), m.getNegbigdec());
+        Assert.assertEquals(new BigInteger("1000"), m.getBigint());
+        Assert.assertEquals(new BigInteger("9223372036854775807000"), m.getBigbigint());
+        Assert.assertEquals(new BigInteger("-1000"), m.getNegbigint());
+        Assert.assertEquals(new BigDecimal("1.0"), m.getBigdec());
+        Assert.assertEquals(new BigDecimal("9223372036854775807000.0123456789"), m.getBigbigdec());
+        Assert.assertEquals(new BigDecimal("-1.0"), m.getNegbigdec());
     }
 }

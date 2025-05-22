bc498caadb77d436b692a49fa442106bcbc6b0ad
Laurent SCHOELENS <61973605+laurentschoelens@users.noreply.github.com>
2025-05-22 17:49:36 +0200

    [#616] fix PropertyListenerInjectorPlugin setters not updated

--- basics/tests/propertylistenerinjector/src/test/java/org/jvnet/jaxb/tests/propertylistenerinjector/AddressTest.java
+++ basics/tests/propertylistenerinjector/src/test/java/org/jvnet/jaxb/tests/propertylistenerinjector/AddressTest.java
@@ -1,16 +1,31 @@
 package org.jvnet.jaxb.tests.propertylistenerinjector;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import generated.Address;
+
+import java.beans.PropertyChangeEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddressTest {
 
     @Test
     public void testAddress() {
+        List<PropertyChangeEvent> events = new ArrayList<>();
+
         Address a = new Address();
-        // just checking new methods have been added
-        a.addPropertyChangeListener(null);
-        a.removePropertyChangeListener(null);
+        a.setStreet("Dollar Lane");
+        a.setCity("Los Angeles");
+
+        a.addPropertyChangeListener("street", events::add);
+        a.setStreet("Penny Lane");
+        a.setCity("New York");
+        Assert.assertEquals(1, events.size());
+        PropertyChangeEvent e = events.get(0);
+        Assert.assertEquals("Dollar Lane", e.getOldValue());
+        Assert.assertEquals("Penny Lane", e.getNewValue());
+        Assert.assertEquals("street", e.getPropertyName());
     }
 }

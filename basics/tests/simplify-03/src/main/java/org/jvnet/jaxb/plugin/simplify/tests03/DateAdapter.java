545687ed1c3460879240d3396d1b88f3f5576e93
Laurent SCHOELENS <61973605+laurentschoelens@users.noreply.github.com>
2025-06-10 10:14:44 +0200

    [#612] remove joda-time from test projects + restore true simplify-02 test

--- basics/tests/simplify-03/src/main/java/org/jvnet/jaxb/plugin/simplify/tests03/DateAdapter.java
+++ basics/tests/simplify-03/src/main/java/org/jvnet/jaxb/plugin/simplify/tests03/DateAdapter.java
@@ -1,33 +1,35 @@
 package org.jvnet.jaxb.plugin.simplify.tests03;
 
 import java.util.Date;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.ISODateTimeFormat;
-
-/**
- *
- *
- * @author
- */
 public class DateAdapter extends XmlAdapter<String, Date> {
 
-	private static final DateTimeFormatter PARSER = ISODateTimeFormat
-			.dateTimeParser().withZoneUTC();
-	private static final DateTimeFormatter FORMATTER = ISODateTimeFormat
-			.dateTime().withZoneUTC();
+	private static final DateTimeFormatter PARSER = new DateTimeFormatterBuilder()
+			.appendPattern("yyyy-MM-dd['T'HH:mm:ss[.SSS][XXX]]")
+			.parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
+			.parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
+			.parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
+			.toFormatter()
+			.withZone(ZoneId.of("UTC"));
+
+	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_DATE_TIME
+			.withZone(ZoneId.of("UTC"));
 
 	@Override
 	public String marshal(Date value) throws Exception {
-		return new DateTime(value).toString(FORMATTER);
+		return FORMATTER.format(value.toInstant());
 	}
 
 	@Override
 	public Date unmarshal(String text) throws Exception {
-		return PARSER.parseDateTime(text).toDate();
+		return Date.from(Instant.from(PARSER.parse(text)));
 	}
 
 }

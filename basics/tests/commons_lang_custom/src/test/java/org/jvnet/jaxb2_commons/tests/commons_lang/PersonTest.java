0cd17552a06f89731604099b01be60208eb2e992
Laurent SCHOELENS <61973605+laurentschoelens@users.noreply.github.com>
2025-09-19 10:29:05 +0200

    fix missing test data in commons_lang_custom project

---
+++ basics/tests/commons_lang_custom/src/test/java/org/jvnet/jaxb2_commons/tests/commons_lang/PersonTest.java
@@ -0,0 +1,20 @@
+package org.jvnet.jaxb2_commons.tests.commons_lang;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import generated.Person;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PersonTest {
+
+    @Test
+    public void testPerson() {
+        Person p = new Person();
+        // No plugin default-value present, checking everything is null or default java value
+        Assertions.assertEquals(false, p.isMailingAddressIdentical());
+
+        Assertions.assertEquals(ToStringBuilder.reflectionToString(p, ToStringStyle.SIMPLE_STYLE), p.toString());
+    }
+}
